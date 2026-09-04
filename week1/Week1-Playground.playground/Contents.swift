d4c2a339b185929109b9b5c764c8d27a7712fd8b
J.R.Han <realryanhan@163.com>
2026-09-04 11:18:04 -0400

    patterns

--- week1/Week1-Playground.playground/Contents.swift
+++ week1/Week1-Playground.playground/Contents.swift
@@ -1,3 +1,17 @@
+
 import UIKit
+import Foundation
 
 var greeting = "Hello, playground"
+
+let str = ["♤", "♧", "♡","♢", " "]
+
+for i in 0..<5 {
+    for j in 0..<10 {
+        for k in 0..<10 {
+            print(str[Int.random(in: 0..<str.count)], terminator:"")
+        }
+        print()
+    }
+    print("\n")
+}
